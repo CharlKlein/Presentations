c71eec645399278a3298f77a835e32611d5fe544
CharlKlein <charl.a.klein@gmail.com>
2022-05-27 23:49:43 +0200

    Added the terraform lock and a Few Comments

--- 2022/CNCJ_External_Secrets_Operator/infra/data.tf
+++ 2022/CNCJ_External_Secrets_Operator/infra/data.tf
@@ -1,7 +1,10 @@
+# Use the Current subscription
 data "azurerm_subscription" "primary" {}
 
+# Use the Current local azure cilent config
 data "azurerm_client_config" "current" {}
 
+# Subnet for the aks cluster 
 data "azurerm_subnet" "sn2" {
   name                 = "sn2"
   virtual_network_name = azurerm_virtual_network.aks_nw.name
@@ -9,6 +12,6 @@ data "azurerm_subnet" "sn2" {
 }
 
 resource "random_id" "app_bytes" {
-  # Generate a new id each time we switch to a new AMI id
+  # Generate a new id for each deployment
   byte_length = 2
 }
